ed3900b1ca666a11e4152915caf3b9d7dd50f9a2
Adisurya690 <Adisurya690@gmail.com>
2025-01-30 03:05:27 +0800

    change logo

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,31 +6,31 @@
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="https://yt3.googleusercontent.com/en8h8L17cHPQ8W13bVWXGmueFdEMBmhc3NyDCxE6LhYXLLOtc8xojWMUl6bWPexmkeeDVfEmb0k=s160-c-k-c0x00ffffff-no-rj" class="h-8 me-3" alt="Logo KPB Gianyar" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">KPB Gianyar</span>
+              <a href="/" class="flex items-center">
+                  <img src="{{ asset('storage/images/logo-KPB.png')}}" class="h-8 me-3" alt="Logo KPB Gianyar" />
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap">KPB Gianyar</span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Menu</h2>
+                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Menu</h2>
                 <ul class="text-gray-500 dark:text-gray-400 font-medium">
                     <li class="mb-4">
-                        <a href="https://flowbite.com/" class="hover:underline">Kebudayaan</a>
+                        <a href="{{ route('kebudayaan') }}" class="hover:underline">Kebudayaan</a>
                     </li>
                     <li class="mb-4">
-                        <a href="https://tailwindcss.com/" class="hover:underline">Blog</a>
+                        <a href="{{ route('blog') }}" class="hover:underline">Blog</a>
                     </li>
                     <li class="mb-4">
-                      <a href="https://tailwindcss.com/" class="hover:underline">Galeri</a>
+                      <a href="{{ route('galeri') }}" class="hover:underline">Galeri</a>
                     </li>
                     <li class="mb-4">
-                      <a href="https://tailwindcss.com/" class="hover:underline">Tentang</a>
+                      <a href="{{ route('tentang') }}" class="hover:underline">Tentang</a>
                     </li>
                 </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Media Sosial</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Media Sosial</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                     <li class="mb-4">
                         <a href="https://instagram.com/kpbgianyar" class="hover:underline" target="_blank">Instagram</a>
@@ -44,7 +44,7 @@
                   </ul>
               </div>
               <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Alamat</h2>
+                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Alamat</h2>
                 <ul class="text-gray-500 dark:text-gray-400 font-medium">
                     <li class="mb-4">
                         <a href="https://flowbite.com/" class="hover:underline">Br. Pande, Pejeng, <br> Tampaksiring, Gianyar</a>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,7 @@
 <nav class="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://yt3.googleusercontent.com/en8h8L17cHPQ8W13bVWXGmueFdEMBmhc3NyDCxE6LhYXLLOtc8xojWMUl6bWPexmkeeDVfEmb0k=s160-c-k-c0x00ffffff-no-rj" class="h-8" alt="Logo KPB Gianyar">
+      <img src="{{ asset('storage/images/logo-KPB.png')}}" class="h-8" alt="Logo KPB Gianyar">
       <span class="self-center text-2xl font-semibold whitespace-nowrap">KPB Gianyar</span>
   </a>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
