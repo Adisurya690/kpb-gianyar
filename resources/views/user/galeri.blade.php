853f08ce0b13798290205012c20a30d0f7c13380
Adisurya690 <Adisurya690@gmail.com>
2025-01-31 17:36:15 +0800

    gallery page user side

--- resources/views/user/galeri.blade.php
+++ resources/views/user/galeri.blade.php
@@ -25,8 +25,29 @@
     </div>
   </div>  
 
+  {{-- Search --}}
+  <div class="my-6">
+    <form class="flex items-center max-w-lg mx-auto">   
+      <label for="simple-search" class="sr-only">Search</label>
+      <div class="relative w-full">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>                    
+          </div>
+          <input type="text" id="simple-search" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full ps-10 p-2.5" placeholder="Cari berdasarkan judul" required />
+      </div>
+      <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-red-700 rounded-lg border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+          <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+          <span class="sr-only">Search</span>
+      </button>
+    </form>
+  </div>
+
   {{-- Card --}}
-  <div class="flex justify-center items-center my-16">
+  <div class="flex justify-center items-center">
     <a href="link-ke-halaman-1" class="max-w-sm bg-white border border-gray-100 rounded-lg shadow mx-3 transform hover:scale-105 hover:bg-gray-50 transition-all">
       <img class="rounded-t-lg object-cover h-48 w-full transition-all" src="https://www.baliekbis.com/wp-content/uploads/2019/07/DSCF9054-800x445.jpg" alt="" />
       <div class="p-5">
