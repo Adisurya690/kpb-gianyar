6ff6a7d42f1ce4f0af0a21ce93182648633da0b2
Adisurya690 <Adisurya690@gmail.com>
2025-02-27 19:49:56 +0800

    font di home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -45,6 +45,11 @@
   </style>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <style>
+    body {
+        font-family: Helvetica, Arial, sans-serif;
+    }
+  </style>
 </head>
 <body>
   {{-- Header --}}
