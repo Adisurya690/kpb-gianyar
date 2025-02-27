f0de3f59857df950a858fd821fcba43dfb72ae9d
Adisurya690 <Adisurya690@gmail.com>
2025-02-27 19:25:15 +0800

    logo navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,7 @@
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <!-- Logo dan Nama Website -->
     <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="{{ asset('storage/images/logo-KPB.png')}}" class="h-8" alt="Logo KPB Gianyar">
+      <img src="{{ asset('storage/app/public/images/logo-KPB.png')}}" class="h-8" alt="Logo KPB Gianyar">
       <span class="self-center text-2xl font-semibold whitespace-nowrap">KPB Gianyar</span>
     </a>
 
