e8d9f247940a77a9893f3cf4567377f89a5419a1
Adisurya690 <Adisurya690@gmail.com>
2025-03-11 20:50:34 +0800

    padding di search kebudayaan

--- resources/views/user/kebudayaan.blade.php
+++ resources/views/user/kebudayaan.blade.php
@@ -45,7 +45,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
           </svg>                                  
         </div>
-        <input type="text" id="simple-search" name="search" class="bg-white border text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full ps-10 p-2.5"
+        <input style="padding-left: 40px" type="text" id="simple-search" name="search" class="bg-white border text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full ps-10 p-2.5"
               value="{{ request('search') }}" placeholder="Cari berdasarkan nama atau kategori atau lokasi" />
       </div>
       <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800">
